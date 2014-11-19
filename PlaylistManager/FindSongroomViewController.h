50ba34cc55ec486a17cde6c760ad9f0e974b22c0
jstevensstein <jstevensstein@gmail.com>
2014-11-19 14:21:25 -0500

    added classes for view controllers

---
+++ PlaylistManager/FindSongroomViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindSongroomViewController.h
+//  PlaylistManager
+//
+//  Created by Joshua Stevens-Stein on 11/19/14.
+//  Copyright (c) 2014 Elliott Ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindSongroomViewController : UIViewController
+
+@end
---
+++ PlaylistManager/PlaylistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistViewController.h
+//  PlaylistManager
+//
+//  Created by Joshua Stevens-Stein on 11/19/14.
+//  Copyright (c) 2014 Elliott Ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistViewController : UIViewController
+
+@end
